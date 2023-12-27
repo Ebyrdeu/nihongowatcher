e840019ee6db8fca35cbea638a41c439669d8d5e
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2023-12-27 16:58:14 +0100

    added dockerfile

--- app/hooks/user-volume.ts
+++ app/hooks/user-volume.ts
@@ -28,38 +28,39 @@ export const userVolume = () => {
     }, [prevValue, videoNode]);
 
     const adjustVolumeBasedOnKey = useCallback((direction: Direction) => {
-        if (videoNode && direction === Direction.LEFT) {
+        if (videoNode && direction === Direction.DOWN) {
             const res = videoNode.volume < 0.1 ? videoNode.volume = 0 : videoNode.volume -= 0.05;
             return setVolume(Number(res.toFixed(2)));
         }
 
-        if (videoNode && direction === Direction.RIGHT) {
+        if (videoNode && direction === Direction.UP) {
             const res = videoNode.volume > 0.9 ? videoNode.volume = 1 : videoNode.volume += 0.05;
             return setVolume(Number(res.toFixed(2)));
         }
 
     }, [videoNode]);
 
+
+    const onWheelVolumeChange = useCallback((event: WheelEvent) => {
+        const direction = event.deltaY < 0 ? Direction.UP : Direction.DOWN;
+        adjustVolumeBasedOnKey(direction);
+    }, [adjustVolumeBasedOnKey]);
+
     useEffect(() => {
         const handleKeyDown = (event: KeyboardEvent) => {
             if (event.key === "m" || event.code === "m") onMuteVolume();
-            if (event.key === "ArrowDown" || event.code === "ArrowDown") adjustVolumeBasedOnKey(Direction.LEFT);
-            if (event.key === "ArrowUp" || event.code === "ArrowUp") adjustVolumeBasedOnKey(Direction.RIGHT);
+            if (event.key === "ArrowDown" || event.code === "ArrowDown") adjustVolumeBasedOnKey(Direction.DOWN);
+            if (event.key === "ArrowUp" || event.code === "ArrowUp") adjustVolumeBasedOnKey(Direction.UP);
         };
 
-        const handleWheel = (event: WheelEvent) => {
-            if (event.deltaY === 100) adjustVolumeBasedOnKey(0);
-            if (event.deltaY === -100) adjustVolumeBasedOnKey(1);
-        };
 
         document.addEventListener("keydown", handleKeyDown);
-        document.addEventListener("wheel", handleWheel);
-
+        document.addEventListener("wheel", onWheelVolumeChange);
         return () => {
             document.removeEventListener("keydown", handleKeyDown);
-            document.removeEventListener("wheel", handleWheel);
+            document.addEventListener("wheel", onWheelVolumeChange);
         };
-    }, [adjustVolumeBasedOnKey, onMuteVolume]);
+    }, [adjustVolumeBasedOnKey, onMuteVolume, onWheelVolumeChange]);
 
     return {
         volume,
