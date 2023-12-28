4ab6d365fb32fc32694ac1966b1fcb42a6661adf
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2023-12-28 16:46:33 +0100

    forgot to delete import from index.ts

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -3,6 +3,5 @@ export * from "./use-toggle-fullscreen";
 export * from "./use-progress";
 export * from "./use-format-video-time";
 export * from "./user-volume";
-export * from "./use-controls-activity";
 export * from "./use-subtitle";
 export * from "./use-idle";
